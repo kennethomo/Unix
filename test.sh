49d0a9a66edaa7602f0f47775eddc09d5bfad780
Kenneth <kennethomo830@gmail.com>
2020-02-26 17:17:29 -0600

    Added a project 3 folder

--- test.sh
+++ test.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-choice=abcd
-#echo $choice | rev
-length=${#choice}
-taco=""
 
-for (( i=$length - 1; i >= 0; i-- )); do
-  taco="$taco${choice:$i:1}"
-done
-echo $taco
+ReverseDir(){
+  choice=$1
+  #echo $choice | rev #This could also work!
+  local length=${#choice}
+  taco=""
 
+  for (( i=$length - 1; i >= 0; i-- )); do
+    taco="$taco${choice:$i:1}"
+  done
+  echo $taco
+}
+
+ReverseDir
 
 exit 0
